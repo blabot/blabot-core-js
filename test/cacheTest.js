bfd99ba6a341790f1813143525e1baef7cc4e5be
Tomas Kuba <tomas.kuba.cz@gmail.com>
2015-10-02 21:06:19 +0200

    Cache branch

--- test/cacheTest.js
+++ test/cacheTest.js
@@ -11,19 +11,19 @@ describe('Cache', function () {
 
   it('should save and load data', function(){
     var payload =['a','b','c','d','e'];
-    (C.load('wrong key') === null).should.be.true;
-    C.save('other data', payload);
-    C.load('other data').should.eql(payload);
+    (C.get('wrong key') === null).should.be.true;
+    C.set('other data', payload);
+    C.get('other data').should.eql(payload);
   });
 
   it('should hold data till expires', function(done){
     var payload = ['a','b','c','d','e'];
-    C.save('payload', payload, 10);
-    C.load('payload').should.eql(payload);
+    C.set('payload', payload, 10);
+    C.get('payload').should.eql(payload);
     setTimeout(function () {
-      C.load('payload').should.eql(payload);
+      C.get('payload').should.eql(payload);
       setTimeout(function () {
-        (C.load('payload') === null).should.be.true;
+        (C.get('payload') === null).should.be.true;
         done();
       }, 10);
     }, 5);
@@ -35,14 +35,14 @@ describe('Cache', function () {
       d = Date.now();
       return d + '-' + c + b + a;
     };
-    C.save('key', function(){
+    C.set('key', function(){
       return fun1('z', 'y', 'x');
     }, 10);
-    C.load('key').should.eql(d + '-xyz');
+    C.get('key').should.eql(d + '-xyz');
     setTimeout(function () {
-      C.load('key').should.eql(d + '-xyz');
+      C.get('key').should.eql(d + '-xyz');
       setTimeout(function () {
-        C.load('key').should.eql(d + '-xyz');
+        C.get('key').should.eql(d + '-xyz');
         done();
       }, 10);
     }, 5);
@@ -50,17 +50,17 @@ describe('Cache', function () {
 
   it('should remove item from cache', function(){
     var payload =['a','b','c','d','e'];
-    C.save('any data', payload);
-    C.load('any data').should.eql(payload);
-    C.remove('any data').should.be.true;
-    (C.load('any data') === null).should.be.true;
+    C.set('any data', payload);
+    C.get('any data').should.eql(payload);
+    C.del('any data').should.be.true;
+    (C.get('any data') === null).should.be.true;
   });
 
   it('should know if key exists', function(){
     var payload =['a','b','c','d','e'];
-    C.save('asdf', payload);
+    C.set('asdf', payload);
     C.has('asdf').should.be.true;
-    C.remove('asdf').should.be.true;
+    C.del('asdf').should.be.true;
     C.has('asdf').should.be.false;
   });
 
